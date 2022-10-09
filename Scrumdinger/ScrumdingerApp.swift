da5f7782019104113124f8908e59c904709c44f7
jscholes <jscholes@cofisoftware.com>
2022-10-09 12:09:31 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,33 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: $store.scrums) {
+                    Task {
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            fatalError("Error saving scrums.")
+                        }
+                    }
+                }
+            }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    fatalError("Error loading scrums.")
+                }
+            }
+        }
+    }
+}
